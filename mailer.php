a71af23ab20a0eeda8cb3ac04553267b4260d0ad
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2016-09-21 19:38:34 -0700

    initial commit

--- mailer.php
+++ mailer.php
@@ -1,31 +1,49 @@
-<?php
-$curl = curl_init();
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://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.sendgrid.com/v3/mail/send",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"dojadeveloper@gmail.com\"\n        }\n      ],\n      \"subject\": \"New Contact\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"[FROM EMAIL]\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"$name<br>$email<br>$subject<br>$message\"\n    }\n  ]\n}",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer [SG API key]",
-    "cache-control: no-cache",
-    "content-type: application/json"
-  ),
-));
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-header('Location: home.html');
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+ "https://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.sendgrid.com/v3/mail/send"
+ <?php
+
+require 'vendor/autoload.php';
+Dotenv::load(__DIR__);
+$sendgrid_apikey = getenv('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');
+$sendgrid = new SendGrid($sendgrid_apikey);
+$url = 'https://api.sendgrid.com/';
+$pass = $sendgrid_apikey;
+$template_id = 'home.html';
+$js = array(
+  'sub' => array(':name' => array('Elmer')),
+  'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
+);
+
+$params = array(
+    'to'        => "dojadeveloper@gmail.com",
+    'toname'    => "Example User",
+    'from'      => "you@youremail.com",
+    'fromname'  => "Your Name",
+    'subject'   => "PHP Test",
+    'text'      => "I'm text!",
+    'html'      => "<strong>I'm HTML!</strong>",
+    'x-smtpapi' => json_encode($js),
+  );
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
+
 ?>
